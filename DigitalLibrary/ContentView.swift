bfddfdef7a1dbab14a8e53a964048a6c82dfb8a3
wongbingg <wb277@naver.com>
2023-05-06 09:00:27 +0900

    Initial Commit

---
+++ DigitalLibrary/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DigitalLibrary
+//
+//  Created by 이원빈 on 2023/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        BookListView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DigitalLibrary/DigitalLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalLibraryApp.swift
+//  DigitalLibrary
+//
+//  Created by 이원빈 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct DigitalLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
