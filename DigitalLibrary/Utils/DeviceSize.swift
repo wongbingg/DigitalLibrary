18d327b853f0fd287c2d8ee5b91715e271c805ad
wongbingg <wb277@naver.com>
2023-05-09 13:21:21 +0900

    feat : "DeviceSize" 네임스페이스 생성

---
+++ DigitalLibrary/Utils/DeviceSize.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceSize.swift
+//  DigitalLibrary
+//
+//  Created by 이원빈 on 2023/05/05.
+//
+
+import UIKit
+
+enum DeviceSize {
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+}
